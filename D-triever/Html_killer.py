298c34500971f5d496d9a374b800abe4773662e2
loneloon <f.kleshev@gmail.com>
2019-09-11 21:39:50 +0300

    html file access commands added

--- D-triever/Html_killer.py
+++ D-triever/Html_killer.py
@@ -1,5 +1,12 @@
+file_name = input("Enter html file name: ")
+
+f= open(file_name, "r")
+text = f.read()
+f.close()
+
 target_atts = ['</td></tr><tr><td class="line-number" value=', '<span class="html-tag">', '<span class="html-attribute-name">',
               '></td><td class="line-content">', '<span class="html-attribute-value">', '<div>', '</div>', '<div', '</div']
 
-#for att in strip_atts:
-#    text = text.replace(att, '')
+for att in strip_atts:
+    text = text.replace(att, '')
+
