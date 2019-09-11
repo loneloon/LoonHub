f247c68c998a2b1940e711aeb9711dc5023109fe
loneloon <f.kleshev@gmail.com>
2019-09-11 21:26:57 +0300

    html_killer attribute list added

--- D-triever/Html_killer.py
+++ D-triever/Html_killer.py
@@ -1,1 +1,5 @@
-#there will be code some day
+target_atts = ['</td></tr><tr><td class="line-number" value=', '<span class="html-tag">', '<span class="html-attribute-name">',
+              '></td><td class="line-content">', '<span class="html-attribute-value">', '<div>', '</div>', '<div', '</div']
+
+#for att in strip_atts:
+#    text = text.replace(att, '')
