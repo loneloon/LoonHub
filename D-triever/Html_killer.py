6713318b5b25e91d1f1a9bd9f3f1e4c73e950e5e
loneloon <f.kleshev@gmail.com>
2019-09-22 21:38:33 +0300

    html_killer: turned code into universal function for html stripping

--- D-triever/Html_killer.py
+++ D-triever/Html_killer.py
@@ -1,25 +1,26 @@
 import urllib.request
 
-fp = urllib.request.urlopen("http://loneloon.net/")
-mybytes = fp.read()
+def html_killer(page: str):
+    fp = urllib.request.urlopen(page)
+    mybytes = fp.read()
 
-text = mybytes.decode("utf8")
-fp.close()
-
-
-target_list = []
-
-def add_att(attr: list):
-    for each in attr:
-        each1 = '<' + each + '>'
-        target_list.append(each1)
-        each2 = '</' + each + '>'
-        target_list.append(each2)
-
-add_att(['tr', 'head', 'body', 'html', 'head', 'h1', 'table', 'title'])
-
-for att in target_list:
-    text = text.replace(att, '')
+    text = mybytes.decode("utf8")
+    fp.close()
 
 
-print(target_list)
+    target_list = []
+
+    def add_att(attr: list):
+        for each in attr:
+            each1 = '<' + each + '>'
+            target_list.append(each1)
+            each2 = '</' + each + '>'
+            target_list.append(each2)
+
+    add_att(['tr', 'head', 'body', 'html', 'head', 'h1', 'table', 'title'])
+
+    for att in target_list:
+        text = text.replace(att, '')
+
+
+    return text
