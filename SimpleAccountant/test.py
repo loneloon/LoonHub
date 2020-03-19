d682522a41df7bc481ef90aff51b3ff8f75276d3
loneloon <f.kleshev@gmail.com>
2020-03-19 21:35:55 +0300

    ===

--- SimpleAccountant/test.py
+++
@@ -1,8 +0,0 @@
-import re
-import string
-
-dob_format = re.compile('(0[1-9]|[12][0-9]|3[01])[.](0[1-9]|1[012])[.](19|20)\d\d')
-email_format = re.compile('(\d|[a-zA-Z])*\@(\d|[a-zA-Z])*\.(\d|[a-zA-Z])*')
-
-print(string.ascii_letters + '-')
-
