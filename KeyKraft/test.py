593564ce0c81639d4dbde6d8e73e83fdcc4e0104
loneloon <f.kleshev@gmail.com>
2020-03-19 14:16:21 +0300

    - implemented interactive folder path selection button

--- KeyKraft/test.py
+++ KeyKraft/test.py
@@ -1,1 +1,15 @@
+import tkinter as tk
+from tkinter import filedialog
+
+root = tk.Tk()
+
+
+def pathdef():
+    print(filedialog.askdirectory())
+
+
+root.button = tk.Button(command=pathdef, width=100)
+root.button.pack()
+
+root.mainloop()
 
