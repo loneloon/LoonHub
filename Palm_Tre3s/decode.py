c194ee20128f4d2dfd502c57e2757bd64d52b7ae
loneloon <f.kleshev@gmail.com>
2020-04-22 18:06:14 +0300

    -2-level encoding algorithm -decoding key generator -decoder

---
+++ Palm_Tre3s/decode.py
@@ -0,0 +1,70 @@
+
+
+class PalmLeaves:
+    def __init__(self):
+
+        from tkinter import filedialog as fd
+
+        self.text = ''
+
+        def pathbutton():
+            return fd.askopenfilename()
+
+        m_path = pathbutton()
+
+        message = open(m_path, 'r')
+        for i in message.readlines():
+            try:
+                self.text += i
+            except IndexError:
+                pass
+
+        print(len(self.text))
+
+        k_path = pathbutton()
+
+        key = {}
+
+        key_f = open(k_path, 'r')
+        for i in key_f.readlines():
+            try:
+                if i[0:4] == 'mix=':
+                    pass
+                elif i[0:-1][-1] in key.values():
+                    pass
+                else:
+                    key[i[0:4]] = i[0:-1][-1]
+            except IndexError:
+                pass
+
+        print(key)
+
+        line = ''
+        broken = []
+        for i in self.text:
+            if '\n' in line:
+                line = line.strip('\n')
+            if len(line) == 4:
+                broken.append(line)
+                line = ''
+            line += i
+
+        if '\n' in line:
+            line = line.strip('\n')
+
+        broken.append(line)
+        line = ''
+
+        print(broken)
+
+        translate = ''
+
+        for i in broken:
+            for j, k in key.items():
+                if i == j:
+                    translate += k
+
+        print(translate)
+
+
+decode = PalmLeaves()
---
+++ Palm_Tre3s/encode.py
@@ -0,0 +1,98 @@
+
+
+class PalmTrees:
+    def __init__(self, text_input):
+        import string
+        import random
+        import datetime
+        # The following is an uneducated attempt of a slow multilevel encoding
+        # The following encoding is layered.
+        # 'maps' stand for code-symbol reference dictionaries at each level
+        # Levels are numbered in ascending order starting with the 1st primary level.
+
+        symbols = string.printable
+
+        self.text_input = text_input
+
+        def spawn_codes(lib: string):
+            codes = []
+            while len(codes) < len(symbols):
+                code = str(random.randint(3789, 7891))
+                if code not in codes:
+                    codes.append(code)
+
+            check = True
+            for i in codes:
+                if codes.count(i) > 1:
+                    check = False
+                    print(i)
+
+            if check:
+                print("Codes generated. No copies!")
+            else:
+                print("Bad result. There are copies!")
+
+            return codes
+
+        primary_codes = spawn_codes(symbols)
+        print(primary_codes)
+
+        def encodelvl1(code_map: list, lib: string):
+            map = {i: j for i, j in zip(code_map, symbols)}
+            print("First lvl of encoding complete. Code map generated!")
+            return map
+
+        map_lvl1 = encodelvl1(primary_codes, symbols)
+        print(map_lvl1)
+
+        def encodelvl2(map_lvl1):
+            dc_string = 'palm_tre3s'
+            transfer_map = {str(idx): lttr for idx, lttr in enumerate(dc_string)}
+            print(transfer_map)
+            main_map = {}
+            for code, symb in map_lvl1.items():
+                temp = []
+                for i in code:
+                    for num, let in transfer_map.items():
+                        if i == num:
+                            temp.append(let)
+                main_map[''.join(temp)] = symb
+
+            if main_map != {}:
+                print("Success! Second level generated.")
+            else:
+                print("Operation failed. Dictionary is empty.")
+
+            return main_map
+
+        map_lvl2 = encodelvl2(map_lvl1)
+        print(map_lvl2)
+
+        timecode = str(datetime.datetime.now()).strip(' ').replace(':', '_')
+
+        key = open(r'C:\Users\User\PycharmProjects\Palm_Tre3s\out\key_%s' % timecode, 'a+')
+
+        for i, j in map_lvl2.items():
+            key.write(f'{i}:{j}\n')
+
+        key.close()
+
+        message = open(r'C:\Users\User\PycharmProjects\Palm_Tre3s\out\message_%s' % timecode, 'a+')
+
+        line = ''
+
+        for i in text_input:
+            for code, symb in map_lvl2.items():
+                if len(line) > 40:
+                    message.write(f'\r{line}')
+                    line = ''
+                if i == symb:
+                    line += code
+
+        message.write(f'{line}')
+        message.close()
+
+
+message = 'I love palm trees. They are serene and loveless.'
+
+encode = PalmTrees(message)
